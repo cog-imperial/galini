c107edcd99cb03c7fc18d5c8dbfc578299aed333
Francesco Ceccon <francesco@ceccon.me>
2018-05-08 14:02:34 +0100

    Remove unused test

--- tests/test_core.py
+++ tests/test_core.py
@@ -58,22 +58,3 @@ def test_children_are_updated(problem):
     assert problem.vertex_depth(s0.idx) == 2
     assert problem.vertex_depth(c0.idx) == 1
     assert problem.size == 4
-
-
-def test_eval_at_x(problem):
-    x = problem.add_variable('x', None, None, Domain.REALS)
-    y = problem.add_variable('y', None, None, Domain.REALS)
-    z = problem.add_variable('z', None, None, Domain.REALS)
-    s0 = SumExpression([0, 1])
-    problem.insert_vertex(s0)
-    p1 = ProductExpression([s0.idx, z.idx])
-    problem.insert_vertex(p1)
-    c0 = problem.add_constraint('c0', p1, None, None)
-    c1 = problem.add_constraint('c1', s0, None, None)
-    h = HessianEvaluator(problem)
-    h.eval_at_x(np.array([1.0, 2.0, 3.0]), True)
-    print('jacobian')
-    print(h.jacobian)
-    print('hessian')
-    print(h.hessian)
-    assert False
