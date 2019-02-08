0999caa6c595768f2427f8322ba4066b95c55d1c
Francesco Ceccon <francesco@ceccon.me>
2019-02-08 14:15:42 +0000

    Test AlphaBB is an underestimator

--- tests/unit/abb/test_underestimator.py
+++ tests/unit/abb/test_underestimator.py
@@ -1,8 +1,10 @@
 # pylint: skip-file
 import pytest
+import numpy as np
 import pyomo.environ as aml
 from suspect.expression import ExpressionType
 from galini.pyomo import dag_from_pyomo_model
+from galini.abb.relaxation import AlphaBBRelaxation
 from galini.abb.underestimator import AlphaBBUnderestimator
 
 
@@ -29,3 +31,16 @@ def test_underestimator(problem):
     assert count_of_type(new_expr, ExpressionType.Product) == 1
     assert count_of_type(new_expr, ExpressionType.Quadratic) == 1
     assert count_of_type(new_expr, ExpressionType.Linear) == 1
+
+
+def test_relaxation(problem):
+    abb = AlphaBBRelaxation()
+    relaxed = abb.relax(problem)
+
+    problem_fg = problem.objectives[0].root_expr.expression_tree_data()
+    relaxed_fg = relaxed.objectives[0].root_expr.expression_tree_data()
+
+    for x in np.linspace(2, 7, 50):
+        problem_fg_x = problem_fg.eval([x]).forward(0, [x])
+        relaxed_fg_x = relaxed_fg.eval([x]).forward(0, [x])
+        assert relaxed_fg_x <= problem_fg_x
