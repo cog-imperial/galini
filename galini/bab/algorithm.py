Fix computation of bab tree lower bound

galini/bab/algorithm.py
@@ -133,6 +133,7 @@ class BabAlgorithm(metaclass=abc.ABCMeta):
                 for child in node_children:
                     tree.add_node(child)
                 continue
+
             logger.info(
                 run_id,
                 'Visiting node {}: parent state={}, parent solution={}',
@@ -160,6 +161,9 @@ class BabAlgorithm(metaclass=abc.ABCMeta):
                 logger.log_prune_bab_node(run_id, current_node.coordinate)
                 continue
 
+            if np.isclose(solution.lower_bound, solution.upper_bound):
+                continue
+
             node_children, branching_point = current_node.branch()
             logger.info(run_id, 'Branched at point {}', branching_point)
             for child in node_children:
@@ -177,14 +181,14 @@ class BabAlgorithm(metaclass=abc.ABCMeta):
             return None
 
     def _solve_problem_at_root(self, run_id, problem, tree, node):
-        self._perform_fbbt(problem)
+        self._perform_fbbt(run_id, problem, tree, node)
         return self.solve_problem_at_root(run_id, problem, tree, node)
 
     def _solve_problem_at_node(self, run_id, problem, tree, node):
-        self._perform_fbbt(problem)
+        self._perform_fbbt(run_id, problem, tree, node)
         return self.solve_problem_at_node(run_id, problem, tree, node)
 
-    def _perform_fbbt(self, problem):
+    def _perform_fbbt(self, run_id, problem, tree, node):
         ctx = detect_special_structure(problem, max_iter=self.fbbt_maxiter)
         for v in problem.variables:
             vv = problem.variable_view(v)
@@ -193,6 +197,9 @@ class BabAlgorithm(metaclass=abc.ABCMeta):
                 new_bound = Interval(None, None)
             vv.set_lower_bound(_safe_lb(new_bound.lower_bound, vv.lower_bound()))
             vv.set_upper_bound(_safe_ub(new_bound.upper_bound, vv.upper_bound()))
+        group_name = '_'.join([str(c) for c in node.coordinate])
+        logger.tensor(run_id, group_name, 'lb', problem.lower_bounds)
+        logger.tensor(run_id, group_name, 'ub', problem.upper_bounds)
 
     def _log_problem_information_at_node(self, run_id, problem, solution, node):
         var_view = node.problem.variable_view(node.variable)

galini/bab/branch_and_cut.py
@@ -20,6 +20,7 @@ from galini.abb.relaxation import AlphaBBRelaxation
 from galini.logging import get_logger
 from galini.core import Constraint, LinearExpression, Domain, Sense
 from galini.cuts import CutsGeneratorsManager
+from galini.util import print_problem
 from galini.config import (
     OptionsGroup,
     NumericOption,
@@ -116,8 +117,17 @@ class BranchAndCutAlgorithm(BabAlgorithm):
                 )
 
             cuts_state.update(mip_solution)
+            if len(new_cuts) == 0:
+                break
 
-        if cuts_state.lower_bound >= tree.upper_bound:
+        logger.info(
+            run_id,
+            'Lower Bound from MIP = {}; Tree Upper Bound = {}',
+            cuts_state.lower_bound,
+            tree.upper_bound
+        )
+
+        if cuts_state.lower_bound >= tree.upper_bound and not np.isclose(cuts_state.lower_bound, tree.upper_bound):
             # No improvement
             return NodeSolution(
                 cuts_state.lower_bound,
@@ -139,7 +149,7 @@ class BranchAndCutAlgorithm(BabAlgorithm):
         )
 
     def _solve_problem_at_root(self, run_id, problem, tree, node):
-        self._perform_fbbt(problem)
+        self._perform_fbbt(run_id, problem, tree, node)
         relaxation, relaxed_problem = self._relax_problem(problem)
         self._cuts_generators_manager.before_start_at_root(run_id, problem, relaxed_problem)
         solution = self.solve_problem_at_root(run_id, problem, relaxed_problem, tree, node, relaxation)
@@ -147,7 +157,7 @@ class BranchAndCutAlgorithm(BabAlgorithm):
         return solution
 
     def _solve_problem_at_node(self, run_id, problem, tree, node):
-        self._perform_fbbt(problem)
+        self._perform_fbbt(run_id, problem, tree, node)
         relaxation, relaxed_problem = self._relax_problem(problem)
         self._cuts_generators_manager.before_start_at_node(run_id, problem, relaxed_problem)
         solution = self.solve_problem_at_node(run_id, problem, relaxed_problem, tree, node, relaxation)
@@ -185,6 +195,11 @@ class BranchAndCutAlgorithm(BabAlgorithm):
                 coefficients.append(-1.0)
                 final_root_expr = LinearExpression(children, coefficients, new_root_expr.constant_term)
                 linear.add_constraint(objective.name, final_root_expr, None, 0)
+            elif root_expr.expression_type == ExpressionType.Variable:
+                new_root_expr = LinearExpression([root_expr, objvar], [1.0, -1.0], 0.0)
+                linear.add_constraint(objective.name, new_root_expr, None, 0)
+            else:
+                raise ValueError('Unknown expression type {}'.format(root_expr.expression_type))
 
         for constraint in problem.constraints:
             root_expr = constraint.root_expr
@@ -196,6 +211,8 @@ class BranchAndCutAlgorithm(BabAlgorithm):
                     continue
                 new_root_expr = _convert_linear_expr(linear, root_expr)
                 linear.add_constraint(constraint.name, new_root_expr, constraint.lower_bound, constraint.upper_bound)
+            else:
+                raise ValueError('Unknown expression type {}'.format(root_expr.expression_type))
         return linear
 
     def _perform_cut_round(self, run_id, problem, relaxed_problem, linear_problem, cuts_state, tree, node):
@@ -207,6 +224,8 @@ class BranchAndCutAlgorithm(BabAlgorithm):
             run_id,
             'Round {}. Linearized problem solution is {}',
             cuts_state.round, mip_solution.status.description())
+        logger.debug(run_id, 'Objective is {}'.format(mip_solution.objectives))
+        logger.debug(run_id, 'Variables are {}'.format(mip_solution.variables))
         assert mip_solution.status.is_success()
 
         # Generate new cuts