b41ca7a51592b2eac83b759909b7f562980a2c76
Francesco Ceccon <francesco@ceccon.me>
2020-07-22 21:30:10 +0100

    fix: register triangle cuts generator

--- setup.py
+++ setup.py
@@ -141,7 +141,8 @@ setup(
             'default=galini.branch_and_cut.primal:DefaultPrimalSearchStrategy',
         ],
         'galini.cuts_generators': [
-            'outer_approximation=galini.outer_approximation:OuterApproximationCutsGenerator'
+            'outer_approximation=galini.outer_approximation:OuterApproximationCutsGenerator',
+            'triangle=galini.triangle:TriangleCutsGenerator'
         ],
     },
     cmdclass={
